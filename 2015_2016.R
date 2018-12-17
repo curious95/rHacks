c5eb499f8b3776004e62f1f861e5fd45ddb82b2f
curious95 <kamalpradhan95@outlook.com>
2018-12-17 14:08:09 +0000

    mlp

--- 2015_2016.R
+++ 2015_2016.R
@@ -53,3 +53,69 @@ t.test(x=df$Price[df$Genre == "ChildrensBooks"],y=df$Price[df$Genre == "Contempo
 # of the mean can be as low as -3.37 and as high as 0.21. The output also produces estimates
 # of the sample means, the mean and the degree of freedom of the t-distribution.
 # Note that Welch’s t-test is a t-test with unequal variances.
+
+
+model3 <- lm(formula = Price ~ NumReviews + Stars + NumPages, data = df)
+summary(model3)
+
+
+
+Y<- as.matrix(df$Price)
+X<-matrix(data=0,ncol = 5,nrow = nrow(df))
+X[1,]
+X[,1]
+X[,1]<-1
+X[,2]<-df$NumReviews
+X[,2]<-df$NumPages
+X[,2]<-df$NumReviews
+X[,3]<-df$NumPages
+X[,4]<-df$Stars
+X[,5]<-df$PublicationYear
+View(X)
+View(Y)
+beta <- ((t(X)*X)^-1)*t(X)Y
+beta <- (inv((t(X)*X))*t(X)Y
+beta <- (inv((t(X)*X))*t(X)*Y
+)
+beta <- ((t(X)*X)^-1)*t(X)*Y
+beta <- ((t(X)%*%X)^-1)*t(X)*Y
+beta <- ((t(X)*X)^-1)*t(X)*Y
+t(X)
+t(X)*X
+t(X)%*%X
+beta <- ((t(X)%*%X)^-1)%*%t(X)%*%Y
+View(beta)
+PY <- X%*%beta
+View(PY)
+plot(PY,col="red")
+line(PY,col="red")
+lines(PY,col="red")
+lines(Y,col="green")
+lines(PY,col="red")
+plot(PY,col="red")
+lines(Y,col="green")
+plot(Y,col="green")
+df2$Y <- Y
+df2 <- data.frame()
+df2$Y<-Y
+df2$Y<-as.data.frame(Y)
+plot(x,y1,type="l",col="red")
+lines(x,y2,col="green")
+plot(Y,type="l",col="red")
+lines(x,y2,col="green")
+lines(PY,col="green")
+plot(type="n",col="red")
+plot(Y,PY,type="n",col="red")
+lines(PY,col="green")
+lines(Y,col="red")
+View(Y)
+beta <- (solve(t(X)%*%X))%*%t(X)%*%Y
+PY <- X%*%beta
+View(PY)
+plot(Y,PY,type="n",col="red")
+lines(PY,col="green")
+plot(Y,PY,type="n",col="red")
+lines(PY,col="green")
+lines(Y,col="red")
+cor(Y,PY)
+
