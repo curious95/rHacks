d0a6a8b7d57eb442aea5ec33d8fb9438e3df1b58
curious95 <kamalpradhan95@outlook.com>
2019-02-12 11:10:39 +0000

    Used Json Lite Package

--- patch_code.R
+++ patch_code.R
@@ -1,14 +1,18 @@
+library(jsonlite)
+
 if(nrow(turtle)==0){
   return(NULL)
 }else{
+  
   #print(turtle$width[nrow(turtle)])
-  
   #resp <- cat("{\"EV.EBIT\":",47,",\"EV.Sales\":",53,"}")
+  #resp <- paste('{"EV.EBIT":', 47, ',"EV.Sales":',57, '}', sep="")
+  #resp <- capture.output(cat('{\"EV.EBIT\":",47,",\"EV.Sales\":",53,"}'))
   
-
-  #paste('{"id":', '"', x, '"', '}', sep="")
   
-  resp <- capture.output(cat('{\"EV.EBIT\":",47,",\"EV.Sales\":",53,"}'))
+  x <- list(EV.EBIT = 47, EV.Sales = 57)
+  resp <- toJSON(x, pretty = TRUE, auto_unbox = TRUE)
+  
   
   print(resp)
 }
