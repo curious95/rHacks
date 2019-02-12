919140357d609bbe84b6b014313fe737d0275ba3
curious95 <kamalpradhan95@outlook.com>
2019-02-12 11:03:41 +0000

    fallback to paste

--- patch_code.R
+++ patch_code.R
@@ -1,11 +1,16 @@
 if(nrow(turtle)==0){
   return(NULL)
 }else{
-  print(turtle$width[nrow(turtle)])
+  #print(turtle$width[nrow(turtle)])
   
-  resp <- cat("{\"EV.EBIT\":",47,",\"EV.Sales\":",53,"}")
+  #resp <- cat("{\"EV.EBIT\":",47,",\"EV.Sales\":",53,"}")
   
+
+  #paste('{"id":', '"', x, '"', '}', sep="")
   
+  resp <- capture.output(cat('{\"EV.EBIT\":",47,",\"EV.Sales\":",53,"}'))
+  
+  print(resp)
 }
 
 #EV.EBIT
